3c84f8a7138482cc132186ee0452ff9ae19d7c3f
Ahmet BASBUNAR <ahmet.basbunar8@etu.univ-lorraine.fr>
2025-05-24 20:55:20 +0200

    Ajout de la configuration de connexion (sans identifiants sensibles)

--- src/modele/connexion.ts
+++ src/modele/connexion.ts
@@ -4,7 +4,8 @@ import * as APIsql from "../modele/sqlWeb.js"
 //APIsql.sqlWeb.init("http://localhost/sae201/vue/", "http://localhost/sae201/IHM_API/")
 
 // En production (à commenter pendant le développement)
-APIsql.sqlWeb.init("https://devweb.iutmetz.univ-lorraine.fr/~e26350u/SSS2SSS/SAE201/SAE201/vue","https://devweb.iutmetz.univ-lorraine.fr/~e26350u/IHM_API/")
+APIsql.sqlWeb.init("https://devweb.iutmetz.univ-lorraine.fr/~e26350u/SSS2SSS/SAE201/SAE201/vue",
+    "https://devweb.iutmetz.univ-lorraine.fr/~e26350u/IHM_API/")
 
 class Connexion {
     constructor() {
@@ -15,7 +16,7 @@ class Connexion {
         //APIsql.sqlWeb.bdOpen('localhost','3306','e26350u_SAE201', 'root','', 'utf8');
         
         // Connexion à la base de données distante (à commenter pendant le développement local)
-        APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','e26350u_SAE201', 'e2635Ou_appli','32407595', 'utf8');
+        APIsql.sqlWeb.bdOpen('devbdd.iutmetz.univ-lorraine.fr','3306','e26350u_SAE201', 'e26350u_appli','32407595', 'utf8');
     }
 }
 const connexion = new Connexion;
